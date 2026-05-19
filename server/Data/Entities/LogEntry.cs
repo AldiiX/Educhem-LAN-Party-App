5e1a42ec96478acd0ae77c0284bef62a2683dbba
Serhii Yavorskiy <yavorskiyserhiy158@gmail.com>
2026-05-19 13:11:47 +0200

    FEAT: Add LogEntry entity and LogType enum
    Introduce a new LogEntry entity mapped to public.Logs (with [UuidV7] and inheriting AuditableEntity<Guid]) to persist application logs. Adds properties: Number, Type (LogType, default INFO), ExactType (max length 32, default "basic"), Message (max length 256), and Date (default DateTime.UtcNow). Also adds the LogType enum (INFO, WARNING, ERROR).

---
+++ server/Data/Entities/LogEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using server.Data.Attributes;
+
+namespace server.Data.Entities;
+
+[UuidV7]
+[Table("Logs", Schema = "public")]
+public class LogEntry : AuditableEntity<Guid>
+{
+    public LogType Type { get; set; } = LogType.INFO;
+
+    [MaxLength(32)]
+    public string ExactType { get; set; } = "basic";
+
+    [MaxLength(256)]
+    public string Message { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+}
+
+public enum LogType
+{
+    INFO,
+    WARNING,
+    ERROR,
+}
