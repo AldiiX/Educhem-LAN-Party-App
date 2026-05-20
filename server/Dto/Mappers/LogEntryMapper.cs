1cf008b31facc8d09ccd39ee7ae8877b40247776
Serhii Yavorskiy <yavorskiyserhiy158@gmail.com>
2026-05-20 10:04:07 +0200

    Create LogEntryMapper.cs

---
+++ server/Dto/Mappers/LogEntryMapper.cs
@@ -0,0 +1,17 @@
+﻿using server.Data.Entities;
+
+namespace server.Dto.Mappers;
+
+public static class LogEntryMapper {
+    extension(LogEntry log) {
+        public LogEntryDto ToDto() {
+            return new LogEntryDto {
+                Id = log.Id,
+                CreatedAtUtc = log.CreatedAtUtc,
+                UpdatedAtUtc = log.UpdatedAtUtc,
+                Date = log.Date,
+                Message = log.Message
+            };
+        }
+    }
+}
